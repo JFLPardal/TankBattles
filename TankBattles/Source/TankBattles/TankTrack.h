dec5d36c16ec834f2a56be9dad6075ad2930a13f
JoaoPardal <jflpardal@gmail.comgit config --global user.email jflpardal@gmail.comgit config --global user.email jflpardal@gmail.com>
2019-12-07 16:20:37 +0000

    TB52 - Setup Tank Throttles for Controller

---
+++ TankBattles/Source/TankBattles/TankTrack.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTrack is used to move the tank and set the maximum drive force
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class TANKBATTLES_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+};

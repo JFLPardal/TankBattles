584b4cd0d7f8592f3db07a38a1f9696db0b2a0bb
JoaoPardal <jflpardal@gmail.comgit config --global user.email jflpardal@gmail.comgit config --global user.email jflpardal@gmail.com>
2019-12-27 17:01:43 +0000

    TB102 - Attachments, Actors and Components (Springs added to the Tank but spawning in the origin of the world)

--- TankBattles/Source/TankBattles/SprungWheel.cpp
+++ TankBattles/Source/TankBattles/SprungWheel.cpp
@@ -12,21 +12,28 @@ ASprungWheel::ASprungWheel()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Physics Contraints"));
+	SetRootComponent(PhysicsConstraint);
+
 	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
-	SetRootComponent(Mass);
+	Mass->SetupAttachment(PhysicsConstraint);
 
 	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
-	Wheel->SetupAttachment(Mass);
-
-	PhysicsConstraint = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Physics Contraints"));
-	PhysicsConstraint->SetupAttachment(Mass);
+	Wheel->SetupAttachment(PhysicsConstraint);
 }
 
 // Called when the game starts or when spawned
 void ASprungWheel::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	if (GetAttachParentActor())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("parent actor not Null"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("parent actor Null"));
+	}
 }
 
 // Called every frame
